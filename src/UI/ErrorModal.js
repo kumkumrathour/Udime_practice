5c2566d45ae41529de54a766cceff213627492a0
kumkum <kumkumrathour00@gmail.com>
2022-11-15 17:29:21 +0530

    add

--- src/UI/ErrorModal.js
+++ src/UI/ErrorModal.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React  from "react";
+import  ReactDOM  from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
-import classes from './ErrorModal.module.css;'
-const ErrorModal = props =>{
-    return <div>
-         <Card className={classes.modal}>
-            <header className={classes.modal}>
-                <h2>{props.title}</h2>
-            </header>
-            <div className={classes.content}>
-                <p>{props.message}</p>
-            </div>
-            <footer className={classes.actions}>
-                <Button> Okey</Button>
-            </footer>
-         </Card>
+import classes from './ErrorModal.module.css'
+
+const Backdrop = props =>{
+    return <div className={classes.backdrop} onClick={props.onConfirm} />
+}
+const ModalOverlag = props =>{
+    return <Card className={classes.modal}>
+    <header className={classes.header}>
+        <h2>{props.title}</h2>
+    </header>
+    <div className={classes.content}>
+        <p>{props.message}</p>
     </div>
+    <footer className={classes.actions}>
+        <Button onClick={props.onConfirm} > Okey</Button>
+    </footer>
+ </Card>
+}
+
+const ErrorModal = props =>{
+    return (
+        <React.Fragment>
+              {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm}/> , 
+              document.getElementById('backdrop-root'))}
+              {ReactDOM.createPortal(<ModalOverlag title={props.title} message={props.message} onConfirm={props.onConfirm}/>,
+              document.getElementById('overlay-root')
+              )}
+        </React.Fragment>
+    )
 }
 export default ErrorModal;
--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,22 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState , useRef} from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css'
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../Helpers/Wrapper';
 const AddUser = (props)=>{
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
+
     const [enteredUsername , setEnteredUsername] = useState('');
     const [enteredUserage , setEnteredUserage] = useState('');
+    const [error , setError] = useState();
+
     const addsubmitHandler = (event) =>{
           event.preventDefault();
-          if(enteredUsername.trim().length === 0 || enteredUserage.trim().length ===0 ){
+          // console.log(nameInputRef.current.value ,ageInputRef);
+          const enteredName = nameInputRef.current.value;
+          const  enteredUserAge = ageInputRef.current.value;
+          if(enteredName.trim().length === 0 || enteredUserAge.trim().length ===0 ){
+            setError({
+              title:'valid error',
+              message :'please enter the valid name '
+            })
             return ;
           }
 
-          if(+enteredUserage<1){
+          if(+enteredUserAge<1){
+            setError({
+              title:'valid age ',
+              message:'please entered a valid age'            })
             return ;
           }
        console.log(enteredUsername ,enteredUserage)
-          props.onAddUser(enteredUsername ,enteredUserage);
+          props.onAddUser(enteredName ,enteredUserAge);
           setEnteredUsername("");
           setEnteredUserage("");
    }
@@ -26,19 +43,31 @@ const AddUser = (props)=>{
    const userageChangeHandler = (event) =>{
     setEnteredUserage(event.target.value);
    };
+   const errorHandler = () =>{
+    setError(null);
+   }
     return (
-        <div>
-            <ErrorModal title="error occure" message="somethink wentwrong"/>
+        <Wrapper>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
         <Card className={classes.input}>
         <form onSubmit={addsubmitHandler}>
              <label htmlFor='username'>Username</label>
-             <input type='text' id="usename" value={enteredUsername} onChange={usernameChangeHandler} ></input>
+             <input type='text' 
+             id="usename" 
+             value={enteredUsername} 
+             onChange={usernameChangeHandler} 
+             ref={nameInputRef}
+             ></input>
              <label htmlFor='age'>Age(Year)</label>
-             <input type='number' id="age" value={enteredUserage} onChange={userageChangeHandler}></input>
+             <input type='number'
+              id="age" value={enteredUserage} 
+              onChange={userageChangeHandler}
+              ref={ageInputRef}
+              ></input>
              <Button type='submit'> Add User </Button>
         </form>
         </Card>
-        </div>
+        </Wrapper>
     )
 }
 export default AddUser ;
